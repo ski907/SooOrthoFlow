f95b9ab1008b7c3005a8879e785cfd309dee1ad1
ski907 <chandler.engel@gmail.com>
2025-12-06 00:37:57 -0500

    make launch_pipeline.bat work for soo_images and sooorthoflow conda names

--- launch_pipeline.bat
+++ launch_pipeline.bat
@@ -1,6 +1,6 @@
 @echo off
-REM --- Ensure conda is initialized ---
-call conda.bat activate soo_images
+REM --- Try soo_images first, then sooorthoflow ---
+call conda.bat activate soo_images 2>nul || call conda.bat activate sooorthoflow
 
 REM --- Run the GUI ---
 python ./pipeline/pipeline_gui.py
